ce81d16a0a80c28f06ee4b31170e5960ba7eaeb3
Timothy Cox <cobalt111@hotmail.com>
2016-11-25 20:54:59 -0500

    Added enqueue, operator =, dequeue, contstructors and destructor, guidelines for the classes in the header files.

--- HW4/HW4/Facebook.h
+++ HW4/HW4/Facebook.h
@@ -5,6 +5,52 @@
 #include <iostream>
 
 
+//Facebook Class:
+//You’re going to #include the Queue class in your Facebook class that will have the
+//following functions and data members:
+
+//Functions:
+
+//1. addFriend(): a given Friend should be added to the Friend list which is a Queue
+//object. That means when you call this function enqueue function from Queue
+//should be triggered and the new Friend should be added to the end of your
+//Queue.
+
+//2. removeFriend(): you don’t have to pass a user name, but whenever this function
+//is called the dequeue function from Queue should be triggered. That means the
+//Friend at the front should be removed from the list and his/her name should be
+//provided.
+
+//3. printFriendsF(): this should trigger the print() function of Queue so that you can
+//print all the Friends from front to the end.
+
+//4. printFriendsR(): this should trigger the reversePrint() function of Queue so that
+//you can print all the Friends from end to the front.
+
+//5. firstFriend(): this should trigger the peek() function of Queue so that you can
+//print the Friend who was added first.
+
+//6. resetFriends(): this should trigger the makeEmpty() function of Queue so that
+//your list can be emptied.
+
+//7. checklist(): this should trigger the isEmpty() function of Queue to verify that your
+//list is empty.
+
+//Data members:
+
+//8. “int numofFriends” to keep track of the total number of Friends
+
+//9. “DataType user” who’s going to be followed.
+
+//10. “Queue<DataType>Friendlist” (this is the Friendlist in a Queue form, not an
+//array!)
+
+
+
+
+
+
+
 // Uses the struct Profile made in earlier assignments
 struct Profile {
 	string user;
--- HW4/HW4/Queue.h
+++ HW4/HW4/Queue.h
@@ -4,12 +4,23 @@
 
 #pragma once
 
+//Queue Class:
+//Write a Queue class using doubly-linked structure and implement the following
+//functionalities.
 
 
+
+
+
+
+// changed the Node struct to handle doubly linked functions
 template <class DataType>
 struct Node {
 	DataType info;
+
 	Node<DataType> *next;
+	// added previous pointer to point to previous Node in list;
+	Node<DataType> *previous;
 };
 
 template <class DataType>
@@ -32,10 +43,18 @@ public:
 	bool isEmpty( ) const;		// returns true if queue is empty
 					// otherwise returns false
 	void makeEmpty( );
+
+	// added these two functions to queue.h
+	void print();
+	void reversePrint();
 private:
 	Node<DataType> *front;
 	Node<DataType> *back;
 	Node<DataType> header;
+
+	// added current pointer to "return" data with
+	Node<DataType> *current;
+
 	inline void deepCopy( const Queue<DataType> & original );
 };
 
@@ -44,54 +63,117 @@ private:
 template<class DataType>
 inline Queue<DataType>::Queue()
 {
-	front = &header;
+	// added initialization
+	front = back = current = &header;
 }
 
 template<class DataType>
 inline Queue<DataType>::Queue(const Queue<DataType> & apqueue)
 {
+	// added deepCopy function
+	deepCopy(apqueue);
 }
 
 template<class DataType>
 inline Queue<DataType>::~Queue()
 {
+	// added makeEmpty
+	makeEmpty();
 }
 
+// overloaded operator to assign queues
 template<class DataType>
 inline Queue<DataType>& Queue<DataType>::operator = (const Queue<DataType> & rqueue)
 {
-	// TODO: insert return statement here
+	if ((*this) == &rqueue)
+		return (*this);
+
+	makeEmpty();
+	deepCopy(rqueue);
+	return (*this);
 }
 
+
+
+
+
+// 1. enqueue (inserts element to the end)
 template<class DataType>
 inline void Queue<DataType>::enqueue(const DataType & element)
 {
 	// create new node to put the enqueued element in
 	Node<DataType> *ptr = new Node<DataType>;
 
-	//
+	// place element's data in the new node
+	ptr->info = element;
+
+	// point the back node's next towards the new pointer made
+	back->next = ptr;
+
+	// move the back pointer so that it points to the end node
+	back = ptr;
 }
 
+
+
+
+// 2. dequeue (removes the front element and provides content)
 template<class DataType>
-inline bool Queue<DataType>::dequeue(DataType & deqElement)
+inline DataType Queue<DataType>::dequeue(DataType & deqElement)
 {
-	return false;
+	// early exit if the queue is empty
+	if (front == back) {
+		return false;
+	}
+	
+	// create pointer prepare for node deletion;
+	Node<DataType> *ptr = front->next;
+	
+	// put ptr->info into location deqElement
+	deqElement = ptr->info;
+
+	// move the header node's next around the element to be deleted
+	front->next = ptr->next;
+
+	// delete the element at front of queue
+	delete ptr;
+	
+	// upon success
+	return true;
 }
 
+
+// 5. peek (provides the element sitting at the top/front, but does not remove)
 template<class DataType>
 inline bool Queue<DataType>::peek(DataType & frontElement)
 {
 	return false;
 }
 
+// 3. isEmpty (checks whether the Queue is empty or not)
 template<class DataType>
 inline bool Queue<DataType>::isEmpty() const
 {
 	return false;
 }
 
+
+// 4. makeEmpty dequeues the entire queue
 template<class DataType>
 inline void Queue<DataType>::makeEmpty()
+{
+}
+
+// 6. print (prints all the elements from front to the end)
+template<class DataType>
+inline void Queue<DataType>::print()
+{
+}
+
+// 7. reversePrint(prints all the elements from end to the front with the help of back
+// pointers inside your nodes)
+template<class DataType>
+inline void Queue<DataType>::reversePrint()
 {
 }
 
